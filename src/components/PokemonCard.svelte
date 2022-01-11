af3245e0654c6097658ea8f980a76ec22676d2a0
Matthew Doles <matteddied@gmail.com>
2022-01-11 09:21:26 -0500

    PomeonCard component

---
+++ src/components/PokemonCard.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let pokemon;
+</script>
+
+<a
+	class="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
+	href={`/pokemon/${pokemon.id}`}
+>
+	<img class="h-40 w-40" src={pokemon.image} alt={pokemon.name} />
+	<h2 class="uppercase">{pokemon.id}. {pokemon.name}</h2>
+</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script>
+	import PokemonCard from '../components/PokemonCard.svelte';
 	import { pokemon } from '../stores/pokestore';
 </script>
 
@@ -8,6 +9,8 @@
 
 <h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
 
-{#each $pokemon as poke}
-	<p>{poke.name}</p>
-{/each}
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+	{#each $pokemon as poke}
+		<PokemonCard pokemon={poke} />
+	{/each}
+</div>
